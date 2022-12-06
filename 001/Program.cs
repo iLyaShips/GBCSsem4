e1e6223126a075aa3b0a89938e910c0dab5ff92d
iLya <andr@google.com>
2022-12-06 23:25:39 +0300

    commit add 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25:
+// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// (в цикле умножение А на А (res=A; res *= A; ), пока i == В)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Degree(int a, int b)
+{
+    int res = a;
+    for (int i = 1; i < b; i++)
+    {
+        res *= a;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат возведения в степень: {Degree(A, B)}");
